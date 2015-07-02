95118595961ab716a7322da8a544827ad2fb3b95
ianofearth <ian.anthony.bevel>
2015-07-02 09:30:52 -0700

    add skill model with content, add skills resource to routes

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Skills do
+describe Skill do
   it { should validate_presence_of :name}
   it { should validate_presence_of :description}
   it { should validate_presence_of :number}
