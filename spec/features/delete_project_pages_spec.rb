f452cc7053479a3899edfe50869d3a4cd1606992
ianofearth <ian.anthony.bevel>
2015-07-02 13:11:58 -0700

    add spec to test delete project

---
+++ spec/features/delete_project_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'the delete project process' do
+  it 'deletes a project' do
+    test_skill = Skill.create({:name => 'random skill', :description => 'random description', :number => 1})
+    test_project = Project.create({:name => 'random project', :description => 'random description', :number => 1})
+    visit skill_project_path
+    click_on 'Delete'
+    expect(page).to have_content 'deleted'
+  end
+end
