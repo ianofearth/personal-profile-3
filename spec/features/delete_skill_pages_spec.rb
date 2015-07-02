81657496ccac4e712c7220f971df21c64ac25fd1
ianofearth <ian.anthony.bevel>
2015-07-02 10:48:49 -0700

    add spec to test delete skill

---
+++ spec/features/delete_skill_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'the delete skill process' do
+  it "deletes a skill" do
+    test_skill = Skill.create({:name => 'delete this skill', :description => 'random description', :number => 1})
+    visit subject_path(test_skill)
+    click_on 'Delete'
+    expect(page).to have_content 'deleted'
+  end
+
+end
