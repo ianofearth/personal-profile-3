b01a73fe5c7ba06cc7a3586949816cd1fe4dbef6
ian bevel <student@epicodus.com>
2015-07-10 11:55:43 -0700

    add spec to test adding new post, pass spec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,24 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @user = current_user
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:notice] = @post.title + ' has been added.'
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
---
+++ spec/features/add_post_page_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "the add question process" do
+  it "creates a new question" do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    click_on 'Log In'
+    fill_in 'Username', :with => 'ian'
+    fill_in 'Password', :with => '1234'
+    click_on 'Sign In'
+    click_on 'View Posts'
+    click_on 'Add Post'
+    fill_in 'Title', :with => 'The Meaning of Life'
+    fill_in 'Body', :with => "I dunno man, it's tough."
+    click_on 'Add Post'
+    expect(page).to have_content "The Meaning of Life has been added."
+  end
+end
